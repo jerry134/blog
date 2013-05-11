c179e65a1668cd993deba985d2acfd6434a7488e
jerry134 <117064092@qq.com>
2013-05-11 16:52:08 +0800

    add grade

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130511041329) do
+ActiveRecord::Schema.define(:version => 20130511085025) do
 
   create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "grades", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
