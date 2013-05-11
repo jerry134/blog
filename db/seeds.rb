dfb020738e0bdfc5ef72e9ac3ef018a8c9820c1e
jerry134 <117064092@qq.com>
2013-05-11 13:24:15 +0800

    add seed to the course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#encoding: utf-8
+
+Course.delete_all
+
+Course.create!([{ name: '语文'}, {name: '数学'}, {name: '英语'}, {name: '物理'}, {name: '化学'}, {name: '历史'}, {name: '生物'}, {name: '地理'}, {name: '思想政治'}])
